fe5a4aad7678a76e021d6de0a8e74a8bfffcc922
rzhvn1 <erzhanmuratov99@gmail.com>
2022-07-05 12:49:17 +0600

    add Feedback and FeedbackType models

---
+++ feedback/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from account.models import CustomUser
+
+
+class FeedbackType(models.Model):
+    name = models.CharField(max_length=55, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Feedback(models.Model):
+    rate_choices = [
+        (1, "1"),
+        (2, "2"),
+        (3, "3"),
+        (4, "4"),
+        (5, "5"),
+    ]
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    type = models.ForeignKey(FeedbackType, on_delete=models.CASCADE)
+    text = models.TextField(blank=True, null=True)
+    rate = models.IntegerField(choices=rate_choices)
+
