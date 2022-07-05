8921e8ed9dc480d67ff679a30e8dbe49d7ef065c
rzhvn1 <erzhanmuratov99@gmail.com>
2022-07-05 20:05:21 +0600

    register about-movie router

--- movie/urls.py
+++ movie/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import MovieModelViewSet
+from .views import MovieModelViewSet, AboutMovieModelViewSet
 
 router = DefaultRouter()
+router.register(r"about-movie", AboutMovieModelViewSet, basename="about-movie")
 router.register(r"", MovieModelViewSet, basename="movie")
+
 
 urlpatterns = [
     path("", include(router.urls)),
