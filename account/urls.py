b4fe13e4633046fd4db129a6dee1546fdf9baf6c
rzhvn1 <erzhanmuratov99@gmail.com>
2022-07-05 11:44:17 +0600

    add ChangePassword and Logout urls

--- account/urls.py
+++ account/urls.py
@@ -2,21 +2,19 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import (
     CustomUserRegisterViewSet,
-    # UpdatePassword,
-    # LogoutView,
-    # CardModelViewSet,
+    UpdatePassword,
+    LogoutView
 )
 from rest_framework_simplejwt import views as jwt_views
 
 
 router = DefaultRouter()
 router.register(r"register", CustomUserRegisterViewSet, basename="register")
-# router.register(r"card", CardModelViewSet, basename="card")
 
 urlpatterns = [
     path("", include(router.urls)),
     path("login/", jwt_views.TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("login/refresh/", jwt_views.TokenRefreshView.as_view(), name="token_refresh"),
-    # path("change-password/", UpdatePassword.as_view(), name="change-password"),
-    # path("logout/", LogoutView.as_view(), name="auth_logout"),
+    path("change-password/", UpdatePassword.as_view(), name="change-password"),
+    path("logout/", LogoutView.as_view(), name="auth_logout"),
 ]
