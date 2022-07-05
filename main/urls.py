0eb53bb08035c12374a320aab8a5b5e824fbaca6
rzhvn1 <erzhanmuratov99@gmail.com>
2022-07-05 12:51:05 +0600

    include feedback.urls

--- main/urls.py
+++ main/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("auth/", include("account.urls")),
+    path('auth/', include('account.urls')),
+    path('feedback/', include('feedback.urls')),
 ]
