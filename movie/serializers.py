11b4e1877a375173b16cb0448229fb55780cfcb1
rzhvn1 <erzhanmuratov99@gmail.com>
2022-07-05 15:06:23 +0600

    add movie_status as SerializerMethodField and get_movie_status

--- movie/serializers.py
+++ movie/serializers.py
@@ -1,15 +1,29 @@
 from rest_framework import serializers
 from .models import Movie, AboutMovie
+import datetime
 
 class MovieSerializer(serializers.ModelSerializer):
 
+    movie_status = serializers.SerializerMethodField(read_only=True)
+
     class Meta:
         model = Movie
-        fields = ['id', 'title', 'age_limit', 'start_date', 'end_date']
+        fields = ['id', 'title', 'age_limit', 'start_date', 'end_date', 'movie_status']
 
         extra_kwargs = {
             "title": {"required":True},
             "age_limit": {"required":True},
             "start_date": {"required":True},
             "end_date": {"required":True},
+            "movie_status": {"required":False}
         }
+
+    @staticmethod
+    def get_movie_status(obj):
+        now = datetime.date.today()
+        if obj.start_date <= now <= obj.end_date:
+            obj.movie_status = 'Current'
+            return obj.movie_status
+        if obj.start_date > now:
+            obj.movie_status = 'Upcoming'
+            return obj.movie_status
