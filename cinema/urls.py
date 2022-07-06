0c82a64975a9b92f6295ce7c4fbee6c2843e8dbe
rzhvn1 <erzhanmuratov99@gmail.com>
2022-07-06 13:03:38 +0600

    register seat router

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CinemaModelViewSet, RoomModelViewSet
+from .views import CinemaModelViewSet, RoomModelViewSet, SeatModelViewSet
 
 router = DefaultRouter()
+router.register(r"seat", SeatModelViewSet, basename="seat")
 router.register(r"room", RoomModelViewSet, basename="room")
 router.register(r"", CinemaModelViewSet, basename="cinema")
 
