92ad504bd25f8f554aadfde0e36fbe6d52ef3b1f
rzhvn1 <erzhanmuratov99@gmail.com>
2022-07-06 13:02:54 +0600

    add SeatSerializer

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Cinema, Room
+from .models import Cinema, Room, Seat
 
 class CinemaSerializer(serializers.ModelSerializer):
 
@@ -14,14 +14,29 @@ class CinemaSerializer(serializers.ModelSerializer):
             "phone": {"required":True}
         }
 
+class SeatSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Seat
+        fields = ['id', 'row_number', 'seat_number', 'room']
+
+        extra_kwargs = {
+            "row_number": {"required":True},
+            "seat_number": {"required":True},
+            "room": {"required":True}
+        }
+
 class RoomSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Room
-        fields = ['id', 'name', 'type', 'cinema']
+        fields = ['id', 'name', 'type', 'cinema', 'seats']
 
         extra_kwargs = {
             "name": {"required":True},
             "type": {"required":True},
             "cinema": {"required":True}
         }
+
+
+
