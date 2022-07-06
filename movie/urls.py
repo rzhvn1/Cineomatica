d9ec5baaa5cbd4397c5a4227512e1a16e8f1c34d
rzhvn1 <erzhanmuratov99@gmail.com>
2022-07-06 13:03:45 +0600

    register showtime router

--- movie/urls.py
+++ movie/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import MovieModelViewSet, AboutMovieModelViewSet
+from .views import MovieModelViewSet, AboutMovieModelViewSet, ShowTimeModelViewSet
 
 router = DefaultRouter()
+router.register(r"showtime", ShowTimeModelViewSet, basename="showtime")
 router.register(r"about-movie", AboutMovieModelViewSet, basename="about-movie")
 router.register(r"", MovieModelViewSet, basename="movie")
 
