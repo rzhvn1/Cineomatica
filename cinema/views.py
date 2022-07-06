a27b2653d6966fd0e1e8094a5ef0b65a52bb1282
rzhvn1 <erzhanmuratov99@gmail.com>
2022-07-06 17:02:45 +0600

    add RoomTypeSerializer

--- cinema/views.py
+++ cinema/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
 from rest_framework import viewsets, permissions
-from .models import Cinema, Room, Seat
-from .serializers import CinemaSerializer, RoomSerializer, SeatSerializer
+from .models import Cinema, Room, Seat, RoomType
+from .serializers import CinemaSerializer, RoomSerializer, SeatSerializer, RoomTypeSerializer
 from .permissions import IsAdminUserOrReadOnly
 
 class CinemaModelViewSet(viewsets.ModelViewSet):
     serializer_class = CinemaSerializer
     permission_classes = [IsAdminUserOrReadOnly]
     queryset = Cinema.objects.all()
+
+class RoomTypeModelViewSet(viewsets.ModelViewSet):
+    serializer_class = RoomTypeSerializer
+    permission_classes = [IsAdminUserOrReadOnly]
+    queryset = RoomType.objects.all()
 
 class RoomModelViewSet(viewsets.ModelViewSet):
     serializer_class = RoomSerializer
