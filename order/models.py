3c6780633e9a349135145f7114fd9c8ca6c71b91
rzhvn1 <erzhanmuratov99@gmail.com>
2022-07-07 13:02:26 +0600

    add TicketType model

--- order/models.py
+++ order/models.py
@@ -8,4 +8,11 @@ class Order(models.Model):
     def __str__(self):
         return f"{self.user}:{self.total_price}"
 
+class TicketType(models.Model):
+    name = models.CharField(max_length=55, unique=True)
+    price = models.PositiveIntegerField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.name}:{self.price}"
+
 
