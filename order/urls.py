ff367acfbc7ab8cfe823f8918325978964c15527
rzhvn1 <erzhanmuratov99@gmail.com>
2022-07-07 21:25:47 +0600

    add ticket url

--- order/urls.py
+++ order/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import OrderModelViewSet, TicketTypeModelViewSet
+from .views import OrderModelViewSet, TicketTypeModelViewSet, TicketModelViewSet
 
 router = DefaultRouter()
+router.register(r"ticket", TicketModelViewSet, basename="ticket")
 router.register(r"ticket-type", TicketTypeModelViewSet, basename="ticket-type")
 router.register(r"", OrderModelViewSet, basename="order")
 
