35ab61014219271d7fc11c4315239023baf6be7d
rzhvn1 <erzhanmuratov99@gmail.com>
2022-07-07 13:03:18 +0600

    register ticket-type router

--- order/urls.py
+++ order/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import OrderModelViewSet
+from .views import OrderModelViewSet, TicketTypeModelViewSet
 
 router = DefaultRouter()
+router.register(r"ticket-type", TicketTypeModelViewSet, basename="ticket-type")
 router.register(r"", OrderModelViewSet, basename="order")
 
 
