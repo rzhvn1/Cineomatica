5e877e010482eca5f4ac483ea57db6a2686e586b
rzhvn1 <erzhanmuratov99@gmail.com>
2022-07-07 21:30:09 +0600

    add TicketModelViewSet and delete perform_create in OrderModelViewSet

--- order/views.py
+++ order/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework import viewsets, permissions
-from .serializers import OrderSerializer, TicketTypeSerializer
-from .models import Order, TicketType
+from .serializers import OrderSerializer, TicketTypeSerializer, TicketSerializer
+from .models import Order, TicketType, Ticket
 from .permissions import IsAdminUserOrReadOnly
 
 class OrderModelViewSet(viewsets.ModelViewSet):
@@ -14,12 +14,15 @@ class OrderModelViewSet(viewsets.ModelViewSet):
             return Order.objects.all()
         return Order.objects.filter(user=self.request.user)
 
-    def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
-
 class TicketTypeModelViewSet(viewsets.ModelViewSet):
 
     serializer_class = TicketTypeSerializer
     permission_classes = [IsAdminUserOrReadOnly]
     queryset = TicketType.objects.all()
+
+class TicketModelViewSet(viewsets.ModelViewSet):
+    serializer_class = TicketSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    queryset = Ticket.objects.all()
+
 
