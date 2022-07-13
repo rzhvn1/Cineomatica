d454c431cb55dafb3131d019ffb21ad7fb33533f
rzhvn1 <erzhanmuratov99@gmail.com>
2022-07-13 20:41:47 +0600

    add created field in Order model

--- order/models.py
+++ order/models.py
@@ -6,6 +6,7 @@ from movie.models import ShowTime
 class Order(models.Model):
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     total_price = models.PositiveIntegerField(default=0, null=True)
+    created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.user}:{self.total_price}"
