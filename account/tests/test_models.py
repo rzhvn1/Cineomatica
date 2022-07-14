e6c92af944bfde4fa471b480d7046f7ac4469829
rzhvn1 <erzhanmuratov99@gmail.com>
2022-07-14 18:19:54 +0600

    add TestClubCardModel

--- account/tests/test_models.py
+++ account/tests/test_models.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from ..models import CustomUser
+from ..models import CustomUser, ClubCard
 
 
 class TestCustomUserModel(TestCase):
@@ -9,3 +9,15 @@ class TestCustomUserModel(TestCase):
             username="rzhvn", email="rzhvn@gmail.com", password="erzhan123", age=23
         )
         self.assertEqual(user.__str__(), user.username)
+
+class TestClubCardModel(TestCase):
+
+    def test_str_method(self):
+        user = CustomUser.objects.create_user(
+            username="rzhvn", email="rzhvn@gmail.com", password="erzhan123", age=23
+        )
+        clubcard = ClubCard.objects.create(
+            user=user
+        )
+        self.assertEqual(clubcard.__str__(), f"{clubcard.balance}")
+
