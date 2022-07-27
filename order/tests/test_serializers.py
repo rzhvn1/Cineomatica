b7feeca029184d5241fd6e1e600623cd5f2952ce
rzhvn1 <erzhanmuratov99@gmail.com>
2022-07-27 21:43:55 +0600

    fix:add TestTicketSerializer

--- order/tests/test_serializers.py
+++ order/tests/test_serializers.py
@@ -22,4 +22,14 @@ class TestTicketSerializer(APITestCase):
         self.url = reverse("ticket-list")
 
     def test_validate_method(self):
+        type = TicketType.objects.create(name="Child", price=50)
+        cinema = Cinema.objects.create(name='Cosmopark')
+        room_type = RoomType.objects.create(name="Demir", price=200)
+        room = Room.objects.create(name="Hall 1", type=room_type, cinema=cinema)
+        seat = Seat.objects.create(row_number=1, seat_number=1, room=room)
+        movie = Movie.objects.create(title="Mad Max 3", age_limit=18, start_date="2022-07-19", end_date="2022-08-19")
+        movie_format = MovieFormat.objects.create(name="3-D", price=100)
+        show_time = ShowTime.objects.create(movie=movie, movie_format=movie_format, room=room, start_time="2022-07-30",
+                                            end_time="2022-08-30")
+        ticket = Ticket.objects.create(type=type, seats=seat, show_time=show_time)
         pass
